58494acd09f8c2552ed9557aecdab6998231d6d8
tanzhi8 <tanzhi.10@qq.com>
2018-12-03 16:55:57 +0800

    删除了一个多余的字符

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -8,52 +8,60 @@ from app.models.user import User
 
 api = Redprint('user')
 
+
 # 获取用户(管理员可访问)
 @api.route('/<int:uid>')
 @auth.login_required
 def admin_get_user(uid):
-    user = User.query.get_or_404(uid)
-    return jsonify(user)
+	user = User.query.get_or_404(uid)
+	return jsonify(user)
+
 
 # 获取用户(所有用户都可以访问)
 @api.route('')
 @auth.login_required
 def get_user():
-    uid = g.user.uid
-    user = User.query.filter_by(id=uid).first_or_404()
-    return jsonify(user)
+	uid = g.user.uid
+	user = User.query.filter_by(id=uid).first_or_404()
+	return jsonify(user)
+
 
 # 删除用户(管理员可访问)
 @api.route('/<int:uid>', methods=['DELETE'])
 @auth.login_required
 def delete_user(uid):
-    with db.auto_commit():
-        user = User.query.filter_by(id=uid).first_or_404()
-        user.delete()
-    return DeleteSuccess()\
+	with db.auto_commit():
+		user = User.query.filter_by(id=uid).first_or_404()
+		user.delete()
+	return DeleteSuccess()
+
 
 # 更新用户(所有用户都可以访问)
 @api.route('', methods=['PUT'])
 @auth.login_required
 def update_user():
-    data = request.json
-    uid = g.user.uid
-    user = User.query.get_or_404(uid)
-    with db.auto_commit():
-        user.nickname = data['nickname']
-    return Success()
+	data = request.json
+	uid = g.user.uid
+	user = User.query.get_or_404(uid)
+	with db.auto_commit():
+		user.nickname = data['nickname']
+	return Success()
+
 
 # 更新用户(管理员可访问)
 @api.route('/<int:uid>', methods=['PUT'])
 @auth.login_required
 def admin_update_user():
-    return 'update qiyue'
+	return 'update qiyue'
+
 
 # 创建一个新的红图
 users = Redprint('users')
+
+
 # 获取用户列表
 @users.route('')
 @auth.login_required
 def admin_get_users():
-    users = User.query.all()
-    return jsonify(users)
+	user_list = User.query.all()
+	return jsonify(user_list)
