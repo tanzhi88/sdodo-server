1f777f9762c7857452602668b0cd3c6382600d29
tanzhi8 <tanzhi.10@qq.com>
2018-12-07 21:11:19 +0800

    定义主域名和静态文件域名

--- app/config/setting.py
+++ app/config/setting.py
@@ -1,6 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
+DEBUG = True
 TOKEN_EXPIRATION = 30 * 24 * 3600
+DOMIAN = 'http://127.0.0.1:5000/'
+DOMAIN_STATIC = 'http://127.0.0.1:5000/static'
 BASE_PATH = basedir
 STATIC_PATH = BASE_PATH + '/static/'
